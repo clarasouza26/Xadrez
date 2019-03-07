a4270ad2c0d92bd0d90d6bb862aa421ac0c0ffd8
Clara Souza <42577323+clarasouza26@users.noreply.github.com>
2019-03-07 13:44:40 -0300

    Add files via upload

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,51 @@
+﻿namespace tabuleiro
+{
+    abstract class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor{ get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+        public int qtdMovimentos { get; protected set; }
+
+        public Peca ( Tabuleiro tab, Cor cor)
+        {
+            this.posicao = null;
+            this.cor = cor;
+            this.tab = tab;
+            this.qtdMovimentos = 0;
+        }
+
+        public void incrementarMovimentos()
+        {
+            qtdMovimentos++;
+        }
+
+        public void decrementarMovimentos()
+        {
+            qtdMovimentos--;
+        }
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i=0; i<tab.linhas; i++)
+            {
+                for (int j=0; j<tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool movimentoPossivel(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+        public abstract bool[,] movimentosPossiveis();
+       
+    }
+}
